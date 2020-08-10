ba2bff9c8a6820b71935486c56cdcf923faa4839
mjpark <gi6238@naver.com>
2020-08-10 22:42:06 +0900

    Random generate color code

--- 01_color-flipper/app.js
+++ 01_color-flipper/app.js
@@ -1,1 +1,10 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = [ 'green', 'red', 'rgba(133,122,200)', '#f15025' ];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+	//Get random number between 0 ~ 3
+	const randomIndex = Math.floor(Math.random() * colors.length);
+	document.body.style.background = colors[randomIndex];
+	color.textContent = colors[randomIndex];
+});
--- 01_color-flipper/hex.js
+++ 01_color-flipper/hex.js
@@ -1,1 +1,14 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F' ];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+btn.addEventListener('click', () => {
+	//Get random number between 0 ~ 3
+	let randomIndex = Math.floor(Math.random() * hex.length);
+	let hexValue = '#';
+	for (let i = 0; i < 6; i++) {
+		randomIndex = Math.floor(Math.random() * hex.length);
+		hexValue += hex[randomIndex];
+	}
+	document.body.style.background = hexValue;
+	color.textContent = hexValue;
+});
